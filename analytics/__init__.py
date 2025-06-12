6cf4e114e929a018d551eadefc762ebcfc04eadb
jonahbandafao <jonah.banda@fao.org>
2025-06-12 15:11:06 +0200

    deleted root folder analytics folder

--- analytics/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-LISWMC Analytics Package
------------------------
-Analytics dashboard and authentication system for 
-Lusaka Integrated Solid Waste Management Company.
-"""
-
-__version__ = "1.0.0"
-__author__ = "LISWMC Development Team" 
